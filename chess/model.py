922422fbe7edcbac215c4b73c7fbac2bd1219560
Guillaume ICHTER <ichter.g@gmail.com>
2021-09-01 12:58:36 +0200

    adding tournament class and db insert

--- chess/model.py
+++ chess/model.py
@@ -20,3 +20,28 @@ class Player(object):
             'elo': self.elo,
         }
         return db.insert(serialized_player)
+
+class Tournament(object):
+    def __init__(self, name, location, date, number_of_turns, 
+                    instances, players, time_control, description):
+        self.name = name
+        self.location = location
+        self.date = date
+        self.number_of_turns = number_of_turns
+        self.instances = instances
+        self.players = players
+        self.time_control = time_control
+        self.description = description
+    
+    def insert_tournament(self):
+        serialized_tournament = {
+            'name': self.name,
+            'location': self.location,
+            'date': self.date,
+            'number_of_turns': self.number_of_turns,
+            'instances': self.instances,
+            'players': self.players,
+            'time_control': self.time_control,
+            'description': self.description,
+        }
+        return db.insert(serialized_tournament)
