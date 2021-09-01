eb161aac68cd39e874221dd971affc40209f9059
Guillaume ICHTER <ichter.g@gmail.com>
2021-09-01 12:58:16 +0200

    quick vocabulary fix

--- chess/view.py
+++ chess/view.py
@@ -4,7 +4,7 @@ def main_menu():
     print("Menu principal")
     print("1: Ajouter un joueur")
     print("2: Créer un tournoi")
-    print("3: Importer un tournoi")
+    print("3: Charger un tournoi")
     print("4: Liste des joueurs enregistrés")
     print("5: Liste des tournois")
     print("0: Quitter")
